84cd943648711f13805e9511adf9b1a77e6d5986
anastasiyapavlova88-hue <anastasiya.pavlova88@gmail.com>
2026-05-08 13:12:40 +0200

    Update 00_data_preparation.sql

--- sql/00_data_preparation.sql
+++ sql/00_data_preparation.sql
@@ -37,3 +37,65 @@ FROM sleep_day;
 SELECT COUNT(DISTINCT Id) AS unique_users
 FROM hourly_steps;
 --показать, что не все пользователи используют все функции
+
+--check dupliates
+SELECT Id, ActivityDate, COUNT(*) AS duplicates
+FROM daily_activity
+GROUP BY Id, ActivityDate
+HAVING COUNT(*) > 1;
+
+SELECT Id, SleepDay, COUNT(*) AS duplicates
+FROM sleep_day
+GROUP BY Id, SleepDay
+HAVING COUNT(*) > 1;
+
+--check NULL values
+SELECT *
+FROM daily_activity
+WHERE TotalSteps IS NULL
+   OR Calories IS NULL
+   OR TotalDistance IS NULL;
+
+SELECT *
+FROM sleep_day
+WHERE TotalMinutesAsleep IS NULL
+   OR TotalTimeInBed IS NULL;
+
+--check 0 values
+SELECT *
+FROM daily_activity
+WHERE TotalSteps = 0;
+
+SELECT *
+FROM daily_activity
+WHERE Calories = 0;
+
+SELECT *
+FROM sleep_day
+WHERE TotalMinutesAsleep = 0;
+
+--amount of days per user
+SELECT 
+    Id,
+    COUNT(DISTINCT ActivityDate) AS active_days
+FROM daily_activity
+GROUP BY Id
+ORDER BY active_days;
+--найти пользователей с очень малым количеством данных.
+
+--check users with less than 10 days
+SELECT *
+FROM (
+    SELECT Id, COUNT(DISTINCT ActivityDate) AS active_days
+    FROM daily_activity
+    GROUP BY Id
+) t
+WHERE active_days < 10;
+--low involvement
+
+--compare the users
+SELECT DISTINCT Id
+FROM daily_activity
+WHERE Id NOT IN (
+    SELECT DISTINCT Id FROM sleep_day
+);
